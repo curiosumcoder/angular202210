136ff2c6a7d79fb19beb5c8312f0d8bcab7bdfc8
Gilberto Bermúdez Garro <gbermude@outlook.com>
2022-11-03 09:45:23 -0600

    Update

--- 20221103/Northwind1/src/app/product/product-edit/product-edit.component.ts
+++ 20221103/Northwind1/src/app/product/product-edit/product-edit.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+
+import { IProduct } from "../../model/iproduct";
+import { ProductService } from "../product.service";
 
 @Component({
-  selector: 'app-product-edit',
-  templateUrl: './product-edit.component.html',
-  styleUrls: ['./product-edit.component.css']
+  selector: "app-product-edit",
+  templateUrl: "./product-edit.component.html",
+  styleUrls: ["./product-edit.component.css"],
+  providers: [ProductService]
 })
 export class ProductEditComponent implements OnInit {
+  item: IProduct = <IProduct>{};
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private ps: ProductService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    // Leer parámetro requerido. Opción #1
+    let id = +this.route.snapshot.params['id'];
+    console.log(`ProductEditComponent: id: ${id}`);
+
+    // Leer parámetro requerido. Opción #2
+    this.route.params.subscribe(params => {
+      let id = params['id'];
+      console.log(`ProductEditComponent: id: ${id}`);
+    });
+
+    // Resolver
+    this.item = this.route.snapshot.data['product'];
+    console.log(this.item);
+
+    // Resolver Observable (misma ruta)
+    //this.route.data.subscribe(data => this.item = data['product']);
   }
 
+  onSave() {
+    console.log('onSave');
+
+    this.ps.update(this.item).subscribe(() => {
+      alert('Successfully update!');
+
+      this.router.navigate(['/product']);
+      //this.router.navigate(['/product'], { queryParamsHandling: 'preserve' });
+    });
+  }
 }
