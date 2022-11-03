417a8f0b6c0cf8eed1bfb00f39b8dd4cace915d2
Gilberto Bermúdez Garro <gbermude@outlook.com>
2022-11-03 09:06:07 -0600

    Update

--- 20221103/Northwind1/src/app/product/product-create-reactive/product-create-reactive.component.ts
+++ 20221103/Northwind1/src/app/product/product-create-reactive/product-create-reactive.component.ts
@@ -1,15 +1,77 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
+
+import { ProductService } from '../product.service';
+
 @Component({
   selector: 'app-product-create-reactive',
   templateUrl: './product-create-reactive.component.html',
-  styleUrls: ['./product-create-reactive.component.css']
+  styleUrls: ['./product-create-reactive.component.css'],
 })
 export class ProductCreateReactiveComponent implements OnInit {
+  // Instancia de grupo de formulario
+  mainForm: FormGroup = this.fb.group({
+    productName: [
+      '',
+      [Validators.required, Validators.minLength(3), Validators.maxLength(50)],
+    ],
+  });
 
-  constructor() { }
+  constructor(
+    private ps: ProductService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
 
-  ngOnInit(): void {
+  // Propiedades
+  get productNameGet() {
+    return this.mainForm.get('productName');
   }
 
+  ngOnInit(): void {
+    console.log('ProductCreateReactiveComponent.Init');
+
+    // // Para limpiar el formulario
+    this.mainForm.reset();
+
+    // Para asignar valores iniciales
+    this.mainForm.patchValue({
+      //productName: 'Demostración',
+    });
+
+    // Suscribirse a cambios
+    this.productNameGet?.valueChanges.subscribe((value) =>
+      console.log(`productName: ${value}`)
+    );
+
+    // Actualizar/Evaluar validadores
+    if (false) {
+      this.productNameGet?.setValidators(Validators.required);
+      this.productNameGet?.clearValidators();
+      this.productNameGet?.updateValueAndValidity();
+    }
+  }
+
+  isValid(name: string): boolean {
+    const fc = this.mainForm.get(name);
+    return fc ? (fc.touched || fc.dirty) && !fc.valid : false;
+  }
+
+  onSave() {
+    console.log('ProductCreateReactiveComponent.OnSave ...');
+    console.log(this.mainForm);
+    this.ps.create(this.mainForm.value).subscribe((product) => {
+      console.log(`Created: ${JSON.stringify(this.mainForm.value)}`);
+      alert('Successfully create!');
+
+      this.router.navigate(['/product']);
+    });
+  }
 }
--- 20221103/Northwind1/src/app/product/product-routing.module.ts
+++ 20221103/Northwind1/src/app/product/product-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ProductIndexComponent } from './product-index/product-index.component';
 import { ProductCreatePlainComponent } from './product-create-plain/product-create-plain.component';
+import { ProductCreateTemplatedComponent } from './product-create-templated/product-create-templated.component';
 
 const routes: Routes = [
   {
     path: 'product',
     children: [
       { path: '', component: ProductIndexComponent },
-      { path: 'create', component: ProductCreatePlainComponent }
+      { path: 'create', component: ProductCreatePlainComponent },
+      { path: 'templated', component: ProductCreateTemplatedComponent }
     ]
   }
 ];
